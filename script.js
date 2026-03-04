b9fd2ade701c9149de6579827766dcd94119f02c
paulojfsf-creator <paulo.jfsf@gmail.com>
2026-03-04 16:24:57 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,54 @@ window.addEventListener("scroll", () => {
 
 if(window.scrollY > 50){
 
-header.style.background="white";
-header.style.color="black";
+header.classList.add("scrolled");
 
 }else{
 
-header.style.background="rgba(0,0,0,0.4)";
-header.style.color="white";
+header.classList.remove("scrolled");
 
 }
 
 });
+
+
+
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+toggle.onclick = () => {
+
+if(nav.style.display === "block"){
+
+nav.style.display="none";
+
+}else{
+
+nav.style.display="block";
+
+}
+
+};
+
+
+
+const images = document.querySelectorAll(".gallery-img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+images.forEach(img => {
+
+img.onclick = () => {
+
+lightbox.style.display="flex";
+lightboxImg.src = img.src;
+
+};
+
+});
+
+lightbox.onclick = () => {
+
+lightbox.style.display="none";
+
+};
